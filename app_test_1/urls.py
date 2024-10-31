74f8510209db473398d85b7bca6373bfac9ef656
nguyenhuuvuong0313 <148206964+huuvuong0123@users.noreply.github.com>
2024-10-31 09:06:40 +0700

    Add files via upload

---
+++ app_test_1/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup_view, name='signup'),
+    path('home/', views.home_view, name='home'),  # URL cho trang home
+    path('calculator/', views.calculator_view, name='calculator'),
+]
